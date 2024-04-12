082b1eaf499e29487a8995040e4a6568948aa730
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-04-12 10:18:06 +0900

    Refactor project settings and add TestClass.java

---
+++ Java-Basic/src/TestClass.java
@@ -0,0 +1,8 @@
+public class TestClass {
+    public static void main(String[] args) throws Exception {
+        int a = 6;
+        int b = 9;
+        int c = a + b;
+        System.out.println("Sum: " + c);
+    }
+}
