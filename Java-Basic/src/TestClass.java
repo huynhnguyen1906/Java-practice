3292be9855fab1e15e777787ddd5cb6143e4e30b
グエン　ドゥック　フィン <duchuynhnguyen1906@gmail.com>
2024-04-12 12:28:22 +0900

    Add variable 'u' and print its value in TestClass.java

--- Java-Basic/src/TestClass.java
+++ Java-Basic/src/TestClass.java
@@ -3,6 +3,9 @@ public class TestClass {
         int a = 6;
         int b = 9;
         int c = a + b;
+        int u;
+        u = 5;
+        System.out.println(u);
         System.out.println("Sum: " + c);
     }
 }
