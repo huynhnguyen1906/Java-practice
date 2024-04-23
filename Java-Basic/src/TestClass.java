c80ed62bd10a7b75b605a31838844a1a430f9eac
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-04-23 15:07:36 +0900

    Add user input functionality to TestClass.java

--- Java-Basic/src/TestClass.java
+++ Java-Basic/src/TestClass.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class TestClass {
     public static void main(String[] args) throws Exception {
         int a = 6;
         int b = 9;
         int c = a + b;
-        int u;
-        u = 5;
-        System.out.println(u);
         System.out.println("Sum: " + c);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Your age is:");
+        int age = scanner.nextInt();
+        System.out.println("Your name is " + name + " and you are " + age + " years old.");
+        scanner.close();
     }
 }
