4af98f67020020b9404c93c03bae29ce273cb1db
NGUYEN DUC HUYNH <duchuynhnguyen1906@gmail.com>
2024-10-08 16:00:14 +0900

    feat: Add Lab02 class for user input and linear equation calculation

---
+++ Java-Basic/src/Lab02.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lab02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // enter number a
+        System.out.println("enter number a:");
+        int a = scanner.nextInt();
+        // enter number b
+        System.out.println("enter number b:");
+        int b = scanner.nextInt();
+        scanner.close();
+        // calculate x
+        if (a > 0) {
+            double x = (double) -b / a;
+            System.out.println("x = " + x);
+        } else if (a == 0) {
+            if (b == 0) {
+                System.out.println("x = all real numbers");
+            } else {
+                System.out.println("x = no solution");
+            }
+        }
+    }
+}
